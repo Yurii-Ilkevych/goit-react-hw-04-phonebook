137b4bae850aec6222664d51a10e880946f674f3
Yurii-Ilkevych <rufiktcl@gmail.com>
2023-05-30 17:26:34 +0300

    add complete task

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
+import { Form, Name, Number, Submit, Wrapper } from './ContactForm.styled';
+function ContactForm({ onForm }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const hundleSubmit = evt => {
+    evt.preventDefault();
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    onForm(contact);
+    resetValue();
+  };
+
+  const handleValue = evt => {
+    const { value } = evt.currentTarget;
+    if (evt.currentTarget.name === 'name') {
+      setName(value);
+    }
+    if (evt.currentTarget.name === 'number') {
+      setNumber(value);
+    }
+  };
+
+  const resetValue = () => {
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <>
+      <Form onSubmit={hundleSubmit}>
+        <Wrapper>
+          <label>
+            Name
+            <Name
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={name}
+              onChange={handleValue}
+            />
+          </label>
+          <label>
+            Number
+            <Number
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              value={number}
+              onChange={handleValue}
+            />
+          </label>
+        </Wrapper>
+        <Submit type="submit">Add contact</Submit>
+      </Form>
+    </>
+  );
+}
+export default ContactForm;
+
+ContactForm.propTypes = {
+  name: PropTypes.string,
+  number: PropTypes.string,
+};
